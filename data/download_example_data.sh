e5e19e44564325e84c3879d5f69315720db8c4b1
Stian Steinbakken <stianste@luke01.idi.ntnu.no>
2019-01-25 10:59:17 +0100

    Use CoLA as example problem instead of MRPC

---
+++ data/download_example_data.sh
@@ -0,0 +1,2 @@
+python download_glue_data.py --data_dir="./GLUE" --tasks="CoLA,SST"
+
--- examples/run_CoLA.sh
+++ examples/run_CoLA.sh
@@ -1,15 +1,15 @@
 export GLUE_DIR=../data/GLUE
 
 python run_classifier.py \
-  --task_name MRPC \
+  --task_name CoLA \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --data_dir $GLUE_DIR/MRPC/ \
+  --data_dir $GLUE_DIR/CoLA/ \
   --bert_model bert-base-uncased \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
   --num_train_epochs 3.0 \
-  --output_dir /tmp/mrpc_output/
+  --output_dir /tmp/cola_output/
 
