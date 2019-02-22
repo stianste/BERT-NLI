affe1a1bd2e0df94e4a31f23a4e8475902b28df1
stianste <stianste@stud.ntnu.no>
2019-02-22 17:32:36 +0100

    Remove redudant argument and add scripts for loading a pre-trained model.

---
+++ scripts/toefl_from_pre_trained.sh
@@ -0,0 +1,13 @@
+export DATA_DIR=./data/NLI-shared-task-2017/
+
+python ./run_BERT_NLI.py \
+  --task_name toefl11 \
+  --do_train \
+  --do_eval \
+  --data_dir $DATA_DIR \
+  --bert_model ./toefl11_models/2019-02-21T14:39:19_seq_128_lower_False_epochs_1.0_lr_3e-05.bin\
+  --max_seq_length 128 \
+  --train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 1 \
+  --output_dir ./toefl11_results
