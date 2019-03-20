79f6639d4ec169643e460ea877325dfd59171166
stianste <stiansteinbakken94@gmail.com>
2019-03-20 13:09:49 +0100

    Verify that some users have more than one chunk

--- tests/verify_downsampling.py
+++ tests/verify_downsampling.py
@@ -6,6 +6,14 @@ def verify_dir(data_dir):
         dir_iterator = os.listdir(f'{data_dir}/{language_folder}')
         assert len(dir_iterator) == 104, f'{language} has {len(dir_iterator)} users.' 
 
+        for username in dir_iterator:
+            num_chunks = len(os.listdir(f'{data_dir}/{language_folder}/{username}'))
+            if len(num_chunks) > 1:
+                return
+
+    assert False, 'All user folders only contains one chunk.'
+
+
 def main():
     verify_dir('./data/RedditL2/reddit_downsampled/europe_data')
     verify_dir('./data/RedditL2/reddit_downsampled/non_europe_data')
