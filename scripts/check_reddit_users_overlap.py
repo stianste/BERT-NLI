4b9d59d4d919cdd33614dc0ae9d0a3c62f336b51
stianste <stiansteinbakken94@gmail.com>
2019-03-20 10:15:34 +0100

    Add script for getting users that are not common in both Europe and non-Europe

---
+++ scripts/check_reddit_users_overlap.py
@@ -0,0 +1,39 @@
+import os
+import logging
+
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt = '%m/%d/%Y %H:%M:%S',
+                    level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+def get_users_from_dir(data_dir: str):
+    usernames = set()
+
+    for label_folder in os.listdir(f'{data_dir}'):
+        label = label_folder.split('.')[1]
+        if label == 'Ukraine':
+            continue # Ignore Ukraine from now, as it is not included in the original article
+
+        for username in os.listdir(f'{data_dir}/{label_folder}'):
+            usernames.add(username)
+
+    return usernames
+
+def get_unique_users():
+    europe_usernames = get_users_from_dir('./data/RedditL2/text_chunks/europe_data')
+    non_europe_usernames = get_users_from_dir('./data/RedditL2/text_chunks/non_europe_data')
+
+    missing_users = set()
+
+    for username in europe_usernames:
+        if username not in non_europe_usernames:
+            missing_users.add(username)
+
+    logger.info(f'Total number of Europe users: {len(europe_usernames)}')
+    logger.info(f'Total number of non Europe users: {len(non_europe_usernames)}')
+    logger.info(f'Total number of missing users: {len(missing_users)}')
+
+    return missing_users 
+
+if __name__ == "__main__":
+    main()
