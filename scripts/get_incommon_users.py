f5063dc615ed9558cd0328facc0a53b6bb25e4a8
stianste <stiansteinbakken94@gmail.com>
2019-03-21 10:59:22 +0100

    Check how many users are common in and out of domain

--- scripts/get_incommon_users.py
+++ scripts/get_incommon_users.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from collections import defaultdict
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt = '%m/%d/%Y %H:%M:%S',
@@ -7,6 +8,7 @@ logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(messa
 logger = logging.getLogger(__name__)
 
 def get_users_from_dir(data_dir: str):
+    lang2usernames = defaultdict(set)
     usernames = set()
 
     for label_folder in os.listdir(f'{data_dir}'):
@@ -16,12 +18,15 @@ def get_users_from_dir(data_dir: str):
 
         for username in os.listdir(f'{data_dir}/{label_folder}'):
             usernames.add(username)
+            lang2usernames[label].add(username)
 
-    return usernames
+    return usernames, lang2usernames
 
-def get_incommon_users():
-    europe_usernames = get_users_from_dir('./data/RedditL2/text_chunks/europe_data')
-    non_europe_usernames = get_users_from_dir('./data/RedditL2/text_chunks/non_europe_data')
+def get_incommon_users(compare_languages=False):
+    europe_usernames, europe_lang2usernames = get_users_from_dir(
+        './data/RedditL2/text_chunks/europe_data')
+    non_europe_usernames, non_europe_lang2usernames = get_users_from_dir(
+        './data/RedditL2/text_chunks/non_europe_data')
 
     incommon_users = set()
 
@@ -32,6 +37,12 @@ def get_incommon_users():
     logger.info(f'Total number of Europe users: {len(europe_usernames)}')
     logger.info(f'Total number of non Europe users: {len(non_europe_usernames)}')
     logger.info(f'Total number of missing users: {len(incommon_users)}')
+
+    if compare_languages:
+        for language, userset in europe_lang2usernames.items():
+            non_europe_user_set = non_europe_lang2usernames[language]
+            common = userset.intersection(non_europe_user_set)
+            logger.info(f'{language} has {len(common)} in and out of domain.')
 
     return incommon_users
 
