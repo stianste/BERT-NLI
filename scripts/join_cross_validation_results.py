f44b2163cd11eda7ad3117800647d3f28927fecd
stianste <stianste@stud.ntnu.no>
2019-03-25 10:42:45 +0100

    Fix join of cross validation and allow to run manually

--- scripts/join_cross_validation_results.py
+++ scripts/join_cross_validation_results.py
@@ -31,29 +31,36 @@ parser.add_argument("--num_train_epochs",
                     help="Total number of training epochs to perform.")
 parser.add_argument("--output_dir",
                     type=str,
-                    required=True,
                     help="The output directory where the model predictions and checkpoints will be written.")
 parser.add_argument("--cross_k",
                     type=int,
-                    required=True,
+                    default=10
                     help="The number of folds to use for cross validation.")
+parser.add_argument("--full_path",
+                    type=str,
+                    default=None,
+                    help="If running manually, this will be used as the full path to the folder.")
 
 args = parser.parse_args()
 
 eval_foldername = get_eval_folder_name(args)
 
-full_path = os.path.join(args.output_dir, eval_foldername)
-results = defaultdict(int)
+if args.full_path:
+    full_path = args.full_path
+else:
+    full_path = os.path.join(args.output_dir, eval_foldername)
+
+results = defaultdict(float)
 
 for fold_result_file in os.listdir(full_path):
     with open(os.path.join(full_path, fold_result_file), 'r') as f:
         for line in f.readlines():
             key, _, value = line.split()
-            results[key] += value
+            results[key] += float(value)
 
 # Average results over k-folds and save to file
 for key in results.keys():
-    value = results[key] / args.cross_k
+    results[key] /= args.cross_k
 
 # Get overall f1 score.
 precision, recall = results['precision'], results['recall']
@@ -61,4 +68,6 @@ results['f1'] = 2 * (precision * recall) / (precision + recall)
 
 with open(os.path.join(full_path, 'final.txt'), 'w') as f:
     for key in sorted(results.keys()):
-      f.write(f'{key} = {value}\n')
+        value = results[key]
+        f.write(f'{key} = {value}\n')
+
