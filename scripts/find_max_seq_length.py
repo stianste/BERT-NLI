c92f16b88a9a76466a811f0409ee20a5576b855e
stianste <stiansteinbakken94@gmail.com>
2019-04-12 10:34:49 +0200

    Find max_seq_length for reddit

--- scripts/find_max_seq_length.py
+++ scripts/find_max_seq_length.py
@@ -1,19 +1,37 @@
 import os
 from pytorch_pretrained_bert.tokenization import BertTokenizer
 
-full_path = './data/NLI-shared-task-2017/data/essays/dev/original/'
+def get_toefl_token_sizes():
+    token_sizes = []
+    full_path = './data/NLI-shared-task-2017/data/essays/train/original/'
+
+    for filename in os.listdir(full_path):
+        with open(os.path.join(full_path, filename), 'r') as f:
+            text = ''.join(f.readlines()).lower()
+
+            tokens = tokenizer.tokenize(text)
+            token_sizes.append(len(tokens))
+
+    return token_sizes
+
+def get_reddit_token_sizes():
+    token_sizes = []
+    data_dir = './data/RedditL2/text_chunks/europe_data'
+
+    for language_folder in os.listdir(data_dir):
+        for username in os.listdir(f'{data_dir}/{language_folder}'):
+            for chunk in os.listdir(f'{data_dir}/{language_folder}/{username}'):
+                full_path = f'{data_dir}/{language_folder}/{username}/{chunk}'
+                with open(full_path, 'r') as f:
+                    text = ''.join(f.readlines()).lower()
+
+                    tokens = tokenizer.tokenize(text)
+                    token_sizes.append(len(tokens))
+
+    return token_sizes
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-
-token_sizes = []
-
-for filename in os.listdir(full_path):
-    example_id = filename.split('.')[0]
-    with open(os.path.join(full_path, filename), 'r') as f:
-        text = ''.join(f.readlines()).lower()
-
-        tokens = tokenizer.tokenize(text)
-        token_sizes.append(len(tokens))
+token_sizes = get_reddit_token_sizes()
 
 print('Max token size', max(token_sizes)) # 910, 747
 
