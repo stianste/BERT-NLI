5393d55f2d551bad62e8e00700614bc3a05388e8
stianste <stiansteinbakken94@gmail.com>
2019-04-12 11:38:18 +0200

    Add classifier for ivectors

---
+++ ivector_classifier.py
@@ -0,0 +1,76 @@
+import numpy as np
+import json
+import logging
+
+from sklearn.neural_network import MLPClassifier
+
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt = '%m/%d/%Y %H:%M:%S',
+                    level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+def load_ivectors(filepath):
+    with open(filepath, 'r') as json_file:
+        data = json.load(json_file)
+        return data
+
+def get_id2label(filepath):
+    id2label = {}
+
+    with open(filepath, 'r') as f:
+        for line in f.readlines():
+            example_id, _, _, label = line.split(',')
+            id2label[example_id.strip()] = label.strip()
+
+    return id2label
+
+def get_examples_and_labels_from_map(data_map, id2label):
+    data = [None for x in range(len(data_map))] 
+    labels = [None for x in range(len(data_map))] 
+
+    for i, guid in enumerate(sorted(data_map.keys())):
+        data[i] = np.array(data_map[guid])
+        labels[i] = id2label[guid]
+
+    return data, labels
+
+def main():
+    training_path = './data/NLI-shared-task-2017/data/features/ivectors/train/ivectors.json'
+    training_labels_path = './data/NLI-shared-task-2017/data/labels/train/labels.train.csv'
+    dev_path = './data/NLI-shared-task-2017/data/features/ivectors/dev/ivectors.json'
+    dev_labels_path = './data/NLI-shared-task-2017/data/labels/dev/labels.dev.csv'
+
+    logger.info('Loading vectors and labels...')
+    training_data_map = load_ivectors(training_path)
+    training_id2labels = get_id2label(training_labels_path)
+
+    logger.info('Merging vectors and labels...')
+    X, y = get_examples_and_labels_from_map(training_data_map, training_id2labels)
+
+    logger.info('Training classifier')
+    model = MLPClassifier(verbose=True)
+    model.fit(X, y)
+
+
+    logger.info('Loading dev data')
+    dev_data_map = load_ivectors(dev_path)
+
+    dev_data_map = load_ivectors(dev_path)
+    dev_id2labels = get_id2label(dev_labels_path)
+
+    logger.info('Merging vectors and labels...')
+    X, y = get_examples_and_labels_from_map(dev_data_map, dev_id2labels)
+
+    logger.info('Evaluating model')
+
+    outputs = model.predict(X)
+    num_correct = sum([1 for i in range(len(outputs)) if outputs[i] == y[i]])
+
+    logger.info('***** Results *****')
+    logger.info(f'{num_correct} out of {len(X)} correct.')
+
+    eval_accuracy = num_correct / len(X)
+    logger.info(f'Accuracy: {eval_accuracy:.3f}%')
+
+if __name__ == '__main__':
+    main()
